697303ad0d32cc850b71f43921e18e73b8cd4b32
AdamAksel <adamcarlssoncoding@gmail.com>
2023-11-21 09:29:06 +0100

    first

---
+++ src/main/java/com/example/todobudget/TodobudgetApplication.java
@@ -0,0 +1,13 @@
+package com.example.todobudget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodobudgetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodobudgetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/todobudget/TodobudgetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.todobudget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodobudgetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
