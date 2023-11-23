9efa579c8fc157df6c7e9b1323c7105a94b0fc38
NenadJovcic <nenad.hif@hotmail.se>
2023-11-23 11:37:48 +0100

    Implemented Expense repository interface

---
+++ src/main/java/expenses/ExpenseRepository.java
@@ -0,0 +1,13 @@
+package expenses;
+
+import org.example.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ExpenseRepository extends JpaRepository<Expense, Long> {
+    List<Expense> findAllByBudgetId(Long budgetId);
+    List<Expense> findAllByUser(User user);
+}
