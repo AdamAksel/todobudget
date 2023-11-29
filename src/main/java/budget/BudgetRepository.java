2001b748239b973c549e1a8dedfa3e9d8854d768
KasperHolmgren <kasperhenrikholmgren@gmail.com>
2023-11-29 19:42:37 +0100

    Added_Repository

---
+++ src/main/java/budget/BudgetRepository.java
@@ -0,0 +1,12 @@
+package budget;
+
+import org.example.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface BudgetRepository extends JpaRepository<Budget, Long> {
+    List<Budget> findAllByUser(User user);
+}
